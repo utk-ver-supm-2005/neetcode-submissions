f1c89dafc6a803627f268f90a19e4e454ca145e7
Utkarsh Verma <utk.ver.official@gmail.com>
2026-04-24 14:12:56 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int req_no_of_times = nums.size()/2 ;
+
+       unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        for(auto p : mp){
+            if(p.second > req_no_of_times)
+                return p.first;
+        }
+        return -1; // do the optimized approach
+    }
+};
