a36483be1972976783020f3584c88830fd36ad51
Utkarsh Verma <utk.ver.official@gmail.com>
2026-05-17 17:09:03 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.cpp
@@ -0,0 +1,32 @@
+class MyHashSet {
+public:
+    set<int> st;
+
+    MyHashSet() {
+    }
+    
+    void add(int key) {
+        st.insert(key);
+    }
+    
+    void remove(int key) {
+        st.erase(key);
+    }
+    
+    bool contains(int key) {
+        if( st.find(key) != st.end() ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);ip
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
